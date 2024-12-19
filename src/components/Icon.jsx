8bdcb9e0acbc47bdb52b28778d2b20e4c881c5dc
devdriven <devdriven>
2024-12-19 18:31:38 -0300

    use BASE_URL to load icon mask dynamically

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,4 +1,4 @@
 export default function Icon({iconType}) {
 
-  return <div className="icon" style={{mask: `url(/react-task-list/${iconType}-icon.svg) no-repeat center`}}></div>
+  return <div className="icon" style={{mask: `url(${import.meta.env.BASE_URL}${iconType}-icon.svg) no-repeat center`}}></div>
 }
